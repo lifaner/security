f2e700f372c0e9fc14ef0fb377a72da4935fcac0
李范 <lifan@feezu.cn>
2018-07-17 17:28:59 +0800

    first

--- src/main/java/com/example/demo/scheduling/test.java
+++ src/main/java/com/example/demo/scheduling/test.java
@@ -1,15 +1,26 @@
 package com.example.demo.scheduling;
 
+import ch.qos.logback.core.util.FixedDelay;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.Operation;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ScheduledFuture;
 
 /**
  * Create by lifan.
  * Date: 2018/7/4.
  * Time: 17:28.
  */
-@Component
+@Slf4j
 public class test {
+
+    @Autowired
+    private ThreadPoolTaskScheduler threadPoolTaskScheduler;
 
 //    @Scheduled(fixedDelay = 1000*10)
     public void sendMessage(){
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,42 @@
 package com.example.demo;
 
+import com.example.demo.mq.ActiveManager;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.activemq.command.ActiveMQQueue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.jms.Destination;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class DemoApplicationTests {
 
+    @Autowired
+    private ActiveManager activeManager;
+
     @Test
-    public void contextLoads() {
+    public void producer() {
+        Destination destination = new ActiveMQQueue("beyondLiQueueTest");
+        //传入队列以及值
+        activeManager.send(destination, "success");
+    }
+
+
+    @Test
+    public void consumer(){
+        class ConsumerClass{
+            @JmsListener(destination = "beyondLiQueueTest")
+            public void con(String info){
+                log.info(info);
+                System.out.println(info);
+            }
+        }
     }
 
 }
